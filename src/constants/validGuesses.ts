bcec307924b82c3ab2a81dea9c8adaa8d03bee79
socarri <106717131+socarri@users.noreply.github.com>
2022-06-02 14:02:15 -0700

    Update validGuesses.ts

--- src/constants/validGuesses.ts
+++ src/constants/validGuesses.ts
@@ -1,6 +1,8 @@
 import { CONFIG } from './config'
 
-export const VALIDGUESSES = [‘laazh’,‘avaan’,‘klosh’,‘ahpoo’,‘tapii’,‘kilot’,‘fleur’,‘batoo’,‘paray’,‘taant’,‘grenn’,‘roozh’,‘salay’,‘balay’,‘playn’,‘plenn’,‘frayr’,‘pchit’,‘machi’,‘miina’,‘swayr']
+export const VALIDGUESSES = [‘laazh’,‘avaan’,‘klosh’,‘ahpoo’,‘tapii’,‘kilot’,‘fleur’,
+                             ‘batoo’,‘paray’,‘taant’,‘grenn’,‘roozh’,‘salay’,‘balay’,
+                             ‘playn’,‘plenn’,‘frayr’,‘pchit’,‘machi’,‘miina’,‘swayr',]
 
 if (CONFIG.normalization) {
   VALIDGUESSES.forEach(
